0b91e34d4eaf405a56863cd4d04dec16e06ce4c4
jaylco <77386599+jaylco@users.noreply.github.com>
2022-10-13 14:23:22 +0100

    updated readme and added deletion flag

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import sys
 from script import run
 from config import LEARNER_RECORD_DATABASE_DATABASE, LEARNER_RECORD_DATABASE_HOST, LEARNER_RECORD_DATABASE_PW, LEARNER_RECORD_DATABASE_USER
 
-run(LEARNER_RECORD_DATABASE_USER, LEARNER_RECORD_DATABASE_PW, LEARNER_RECORD_DATABASE_HOST, LEARNER_RECORD_DATABASE_DATABASE)
+args = sys.argv
+delete_records = False
+
+if (len(args) > 1):
+    delete_records = args[1] == "delete"
+
+print(f"Running with delete records flag set to {delete_records}")
+
+run(LEARNER_RECORD_DATABASE_USER, LEARNER_RECORD_DATABASE_PW, LEARNER_RECORD_DATABASE_HOST, LEARNER_RECORD_DATABASE_DATABASE, delete_records)
